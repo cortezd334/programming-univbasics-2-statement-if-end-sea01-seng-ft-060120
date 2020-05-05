b167020d32044adf0e242ae8413c57767babd595
Danira Cortez <cortezd334@gmail.com>
2020-05-05 03:55:32 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,26 +1,4 @@
 
-run_code_inside = false
-
-#puts "Code before if...end"
-#if run_code_inside
- # puts "code inside"
-#end
-#puts "Code after if...end"
-
-chance_of_rain = -23
-
-if chance_of_rain <= 0.25
-  puts "Pack a sun shelter!"
-elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  puts "Pack an umbrella!"
-else
-  puts "Stay home and read Hegel."
-end
-
-
-this_year = Time.now.year
-puts "Hey, it's #{this_year}"
-
 current_time = Time.now
 
 if current_time.to_i / 2
